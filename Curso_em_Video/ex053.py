88c9f90e24233cb3e857890731312ef491bb7a2d
Gabriel Afonso Marchesi Lopes <marchesilopes@gmail.com>
2023-10-23 14:42:37 -0300

    Novos exercícios incluídos.

---
+++ Curso_em_Video/ex053.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia uma frase qualquer e diga se ela é palíndromo, desconsiderando os espaços e acentos.
+# APOS A SOPA
+# A SACADA DA CASA
+# A TORRE DA DERROTA
+# O LOBO AMA O BOLO
+# ANOTARAM A DATA DA MARATONA
