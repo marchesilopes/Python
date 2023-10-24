2656762e24416578f6393532a13a811e96b528e0
Gabriel Afonso Marchesi Lopes <marchesilopes@gmail.com>
2023-10-24 13:30:17 -0300

    Exemplo de análise de dados

---
+++ Hashtag/Analise_de_Dados/venda.py
@@ -0,0 +1,22 @@
+# INTRODUÇÃO A ANÁLISE DE DADOS
+# Como funciona um Projeto de Análise de DAdos?
+# Os projetos de Análise de dados são na verdade Desafios das Empresas
+
+# DESAFIO
+# - Empresa vende bermudas
+# - 5 Lojas
+# - Está querendo aumentar as vendas
+# O que fazer?
+# Informações Disponíveis: Base de Vendas
+
+# Passo 1 - Trazer sua base de dados para o Python e ver o que tem nela
+import pandas as pd
+
+tabela = pd.read_excel("C:/Users/gabriel.marchesi/Desktop/Estatística/Python/pythonProject/GitHub/Python/Hashtag/Analise_de_Dados/Vendas.xlsx")
+print(tabela)
+
+# Passo 2 - Pegar um panorama geral sobre a sua base de dados
+
+# Passo 3 - Começar sua análise Top->Down
+
+# Passo 4 - Entrar no detalhe para entender
