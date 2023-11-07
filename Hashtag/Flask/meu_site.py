55d745a0e58a06d083a18b413d446dbda5a9c66d
Gabriel Afonso Marchesi Lopes <marchesilopes@gmail.com>
2023-11-07 10:47:02 -0300

    Incluído exemplo de Flask

---
+++ Hashtag/Flask/meu_site.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# criar a 1ª página do site
+# route -> qual é o caminho que vem depois de seu domínio, se for só uma "/", será a homepage
+# função -> o que você quer exibir naquela página
+# template
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
