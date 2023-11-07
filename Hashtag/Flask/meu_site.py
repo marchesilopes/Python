91a9c852efed6eecc2ac12c400be1982ec4b4e49
Gabriel Afonso Marchesi Lopes <marchesilopes@gmail.com>
2023-11-07 15:34:45 -0300

    Incluído exemplo de Flask

--- Hashtag/Flask/meu_site.py
+++ Hashtag/Flask/meu_site.py
@@ -15,6 +15,10 @@ def homepage():
 def contatos():
     return render_template("contatos.html")
 
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
