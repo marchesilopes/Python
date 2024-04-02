a654e2ae33d8fdb579f667ff5f7a648f0e0b6609
Gabriel Afonso Marchesi Lopes <marchesilopes@gmail.com>
2024-04-02 12:17:57 -0300

    Update ex013.py

--- Curso_em_Video/Exercicios/ex013.py
+++ Curso_em_Video/Exercicios/ex013.py
@@ -1,1 +1,3 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+s = float(input('Digite o salário em reais: '))
+print('O valor do salário é {:.2f}. Com um aumento de 15%, o novo salário será de {:.2f} reais'.format(s, s*1.15))
