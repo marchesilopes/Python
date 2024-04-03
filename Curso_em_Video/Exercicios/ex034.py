392f69457a67bb7d7c030bb9ce5cabbec259a95f
Gabriel Afonso Marchesi Lopes <marchesilopes@gmail.com>
2024-04-03 16:17:02 -0300

    Atualização

--- Curso_em_Video/Exercicios/ex034.py
+++ Curso_em_Video/Exercicios/ex034.py
@@ -1,3 +1,9 @@
 # Escreva um programa que pergunte o salário de um funcionário e calcule o valor de seu aumento.
 # Para salários superiores a R$1.250,00, calcule um aumento de 10%.
 # Para os inferiores ou iguais, o aumento é de 15%.
+s = float(input('Digite o salário do funcionário em reais: '))
+
+if s >= 1250:
+    print('O funcionário terá um aumento de 10%' 'e o valor de seu novo salário será {:.2f}'.format(s*1.1))
+else:
+    print('O funcionário terá um aumento de 15%' 'e o valor de seu novo salário será {:.2f}'.format(s*1.15))
--- Curso_em_Video/Exercicios/ex035.py
+++ Curso_em_Video/Exercicios/ex035.py
@@ -1,1 +1,12 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo
+r1 = float(input('Digite o comprimento da reta 1: '))
+r2 = float(input('Digite o comprimento da reta 2: '))
+r3 = float(input('Digite o comprimento da reta 3: '))
+
+if r1 < (r2 + r3):
+    if (r1 + r2) > r3:
+        print('As retas formam um triângulo')
+    else:
+        print('As retas NÃO formam um triângulo')
+else:
+    print('As retas NÃO formam um triângulo')
