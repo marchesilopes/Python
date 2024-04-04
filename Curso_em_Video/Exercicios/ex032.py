25caaf8e8c3f94982c547fa37bc4130e644b31b7
Gabriel Afonso Marchesi Lopes <marchesilopes@gmail.com>
2024-04-04 08:46:48 -0300

    Atualização

--- Curso_em_Video/Exercicios/ex032.py
+++ Curso_em_Video/Exercicios/ex032.py
@@ -1,1 +1,10 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+ano = int(input('Digite um ano: '))
+
+if (ano % 400) == 0:
+    print('O ano é bissexto!')
+else:
+    if (ano % 4) == 0 and (ano % 100) != 0:
+        print('O ano é bissexto!')
+    else:
+        print('O ano NÃO é bissexto!')
